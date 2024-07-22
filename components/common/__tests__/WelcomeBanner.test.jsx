64d9021c6870bae1c815695e2c95f258b67e4bee
Madhan Kumar <madhankumar@bicsglobal.com>
2024-07-22 15:39:17 +0530

    add test for welcome banner

---
+++ components/common/__tests__/WelcomeBanner.test.jsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import WelcomeBanner from "../WelcomeBanner";
+import { mockImages } from "../mockTestData/ImageList.mock";
+
+test('it renders welcome banner content', () => {
+    render(<WelcomeBanner image={mockImages}/>);
+});
+
+test('it renders images in welcome banner content', () => {
+    render(<WelcomeBanner image={mockImages}/>);
+
+    const images = screen.getAllByRole('img');
+    expect(images).toHaveLength(3)
+});
