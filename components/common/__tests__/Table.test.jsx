6553f51cfabcc2a2f3592202a8393b674e2c6c7d
Madhan Kumar <madhankumar@bicsglobal.com>
2024-07-22 16:50:20 +0530

    add test script for table component

---
+++ components/common/__tests__/Table.test.jsx
@@ -0,0 +1,23 @@
+import { getByText, render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event'
+import Table from '../Table';
+import { mockTableData } from '../__mocks__/Table.mock';
+
+test('it displays all rows and columns in table', () => {
+    render(<Table rows={mockTableData.rows} columns={mockTableData.columns}/>);
+});
+
+test('user navigates to order details page', () => {
+    render(<Table rows={mockTableData.rows} columns={mockTableData.columns}/>)
+
+    const viewMoreLinks = screen.getAllByRole('link');
+    user.click(viewMoreLinks[0])
+});
+
+test('it displays message on empty rows', () => {
+    render(<Table rows={[]} columns={mockTableData.columns} />);
+
+    const table = screen.getByRole('table');
+    const message = getByText(table, 'No orders found');
+    expect(message).toBeTruthy();
+})
